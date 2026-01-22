50193c93e0f66e4a62dd3d605daae3e21a93868b
MetsysLlifer <metsysllifer@gmail.com>
2026-01-22 10:07:37 +0800

    Add Bubble Sort implementation with array population and display functions
    - Created main.c to implement Bubble Sort algorithm.
    - Added functions to populate an array with random integers and display the array.
    - Implemented a makefile for building and running the program.

---
+++ Sorts/Bubble/main.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define MAX 10
+
+// Bubble Sort pushes highest value in the array to the right every iteration.
+// Once the highest value pushed to the right most index, decrement the size by 1 and loop again
+// until it's sorted.
+
+void populateArray(int* arr, int size);
+void displayArray(int* arr, int size);
+void bubbleSort(int* arr, int size);
+
+int main(){
+    int arr[MAX];
+    populateArray(arr, MAX);
+    bubbleSort(arr, MAX);
+
+    displayArray(arr, MAX);
+    return 0;
+}
+
+
+void populateArray(int* arr, int size){
+    for(int trav = 0; trav < size; trav++){
+        arr[trav] = rand() % size + 1;
+    }
+}
+
+
+void displayArray(int* arr, int size){
+    for(int trav = 0; trav < size; trav++){
+        printf("[%d]: %d\n", trav, arr[trav]);
+    }
+}
+
+void bubbleSort(int* arr, int size){
+    for(int travA = 0; travA < size-1; travA++){
+        for(int travB = 0; travB < size-travA; travB++){
+            if(arr[travB] > arr[travB+1]){
+                int temp = arr[travB];
+                arr[travB] = arr[travB+1];
+                arr[travB+1] = temp;
+            }
+        }
+    }
+}
